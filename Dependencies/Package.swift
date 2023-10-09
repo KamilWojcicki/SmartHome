0ff19adb7b48d332daf18061b698fc947a59ebd0
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2023-10-09 09:17:00 +0200

    Add dependencies package

---
+++ Dependencies/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Dependencies",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "Dependencies",
+            targets: ["Dependencies"]),
+    ],
+    dependencies: [
+        .package(path: "Root")
+    ],
+    targets: [
+        .target(
+            name: "Dependencies",
+            dependencies: [
+                .product(
+                    name: "Root",
+                    package: "Root"
+                )
+            ]
+        )
+    ]
+)
