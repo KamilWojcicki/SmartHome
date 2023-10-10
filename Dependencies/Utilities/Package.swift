cd24bf29f40bd4d5f6f3418bf539e2d651407e08
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2023-10-10 13:42:48 +0200

    Add Utilities Package

---
+++ Dependencies/Utilities/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Utilities",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "Utilities",
+            targets: ["Utilities"]
+        )
+    ],
+    dependencies: [
+        
+    ],
+    targets: [
+        .target(
+            name: "Utilities")
+    ]
+)
