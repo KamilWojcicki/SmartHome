86d8adf1d36268887c7b2df468b484b3bdbc2e0b
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2023-10-18 18:32:21 +0200

    Add Home Package

---
+++ Dependencies/Home/Sources/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//
+//
+//  Created by Kamil Wójcicki on 02/10/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
