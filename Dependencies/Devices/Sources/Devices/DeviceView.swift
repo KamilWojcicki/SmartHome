7aa1eb88e86d531b1ee578809cf8a99c306cf50e
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2023-10-18 18:32:11 +0200

    Add Device Package

---
+++ Dependencies/Devices/Sources/Devices/DeviceView.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceView.swift
+//
+//
+//  Created by Kamil Wójcicki on 02/10/2023.
+//
+
+import SwiftUI
+
+struct DeviceView: View {
+    var body: some View {
+        Text("Device View")
+    }
+}
+
+#Preview {
+    DeviceView()
+}
