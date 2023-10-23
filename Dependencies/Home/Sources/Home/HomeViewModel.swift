dd0a38e4e71b72c5406bfdef3e65c3195044cd21
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2023-10-23 14:53:01 +0200

    Add Home Package

--- Dependencies/Home/Sources/Home/HomeViewModel.swift
+++ Dependencies/Home/Sources/Home/HomeViewModel.swift
@@ -5,4 +5,19 @@
 //  Created by Kamil Wójcicki on 02/10/2023.
 //
 
+import AuthenticationInterface
+import DependencyInjection
 import Foundation
+
+final class HomeViewModel: ObservableObject {
+    @Inject private var authenticationManager: AuthenticationManagerInterface
+    
+    func signOut() {
+        do {
+            try authenticationManager.signOut()
+        } catch {
+            print(error.localizedDescription)
+        }
+       
+    }
+}
