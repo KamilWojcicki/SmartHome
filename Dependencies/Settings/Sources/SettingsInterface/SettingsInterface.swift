76e33179b454794367262ec90806a066a3ffb614
Kamil Wójcicki <kamilwojcicki@MacBook-Air-Kamil.local>
2023-11-01 23:09:02 +0100

    Add enum to SettingsInterface

--- Dependencies/Settings/Sources/SettingsInterface/SettingsInterface.swift
+++ Dependencies/Settings/Sources/SettingsInterface/SettingsInterface.swift
@@ -11,3 +11,10 @@ public enum languageOption: String {
     case pl = "Polish"
     case eng = "English"
 }
+
+public enum AuthenticationProviderOption: String {
+    case email      = "password"
+    case google     = "google.com"
+    case apple      = "apple.com"
+    case facebook   = "facebook.com"
+}
