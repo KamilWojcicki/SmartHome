280aeb2ccc6e9db6e5d54dcafdd3ba57ec46074d
Kamil Wójcicki <kamilwojcicki@MacBook-Air-Kamil.local>
2023-11-01 23:08:46 +0100

    Modify User Package

--- Dependencies/User/Sources/User/UserManager.swift
+++ Dependencies/User/Sources/User/UserManager.swift
@@ -6,14 +6,15 @@
 //
 
 import AuthenticationInterface
-import UserInterface
-import SwiftUI
+import CloudDatabaseInterface
 import DependencyInjection
+import SwiftUI
+import UserInterface
 
 final class UserManager: UserManagerInterface {
     
     @Inject private var authenticationManager: AuthenticationManagerInterface
-    
+    @Inject private var cloudDatabaseManager: CloudDatabaseManagerInterface
     private var user: User? {
         didSet {
             userHandler?(user)
@@ -38,8 +39,18 @@ final class UserManager: UserManagerInterface {
         }
     }()
     
+    func getCurrentUser() throws -> User {
+        let firebaseUser = try authenticationManager.getCurrentUser()
+        return User(from: firebaseUser)
+    }
+    
+    func getFirestoreUser() async throws -> User {
+        let firebaseUser = try getCurrentUser()
+        return try await cloudDatabaseManager.read(parentObject: User?.none, object: firebaseUser)
+    }
+    
     func isUserAuthenticated() throws -> Bool{
-       return try authenticationManager.isUserAuthenticated()
+        try authenticationManager.isUserAuthenticated()
     }
     
     func signOut() throws {
@@ -48,13 +59,21 @@ final class UserManager: UserManagerInterface {
     }
     
     func signUp(email: String, password: String) async throws {
-        try await authenticationManager.createUser(email: email, password: password)
+        
+        let authDataResult = try await authenticationManager.createUser(email: email, password: password)
+        try await handleSignUp(authDataResult: authDataResult)
         try await signIn(email: email, password: password)
     }
     
     func signIn(email: String, password: String) async throws {
-        let authDataResult = try await authenticationManager.signInUser(email: email, password: password)
-        self.user = User(from: authDataResult)
+       let authDataResult = try await authenticationManager.signInUser(email: email, password: password)
+        let user = User(from: authDataResult)
+        self.user = user
+    }
+    
+    private func handleSignUp(authDataResult: AuthenticationDataResult) async throws {
+        let user = User(from: authDataResult)
+        try cloudDatabaseManager.create(parentObject: User?.none, object: user)
     }
     
     func updatePassword(email: String, password: String, newPassword: String) async throws {
@@ -69,13 +88,51 @@ final class UserManager: UserManagerInterface {
         try await authenticationManager.deleteAccount()
     }
     
+    func handleFirstLogin() async throws -> Bool {
+        let user = try getCurrentUser()
+        return try await cloudDatabaseManager.handleObjectExist(parentObject: User?.none, object: user)
+    }
+    
+    func checkIsFirstLogin() async throws -> Bool {
+        let user = try await getFirestoreUser()
+        print(user.isFirstLogin)
+        return user.isFirstLogin
+    }
+}
+
+//MARK: SOCIAL MEDIA SIGN IN
+extension UserManager {
     func signInWithGoogle() async throws {
         let authDataResult = try await authenticationManager.signInWithGoogle()
+        
         self.user = User(from: authDataResult)
     }
     
     func signInWithFacebook() async throws {
         let authDataResult = try await authenticationManager.signInWithFacebook()
         self.user = User(from: authDataResult)
+    }
+}
+
+//MARK: MANAGE DATA
+extension UserManager {
+    func create<ParentObject: Storable, Object: Storable>(parentObject: ParentObject?, object: Object) throws {
+        try cloudDatabaseManager.create(parentObject: parentObject, object: object)
+    }
+    
+    func read<ParentObject: Storable, Object: Storable>(parentObject: ParentObject?, object: Object) async throws -> Object {
+        try await cloudDatabaseManager.read(parentObject: parentObject, object: object)
+    }
+    
+    func readAll<ParentObject: Storable, Object: Storable>(parentObject: ParentObject?, objectsOfType type: Object.Type) async throws -> [Object] {
+        try await cloudDatabaseManager.readAll(parentObject: parentObject, objectsOfType: type)
+    }
+    
+    func update<ParentObject: Storable, Object: Storable>(parentObject: ParentObject?, object: Object, data: [String: Any]) throws {
+        try cloudDatabaseManager.update(parentObject: parentObject, object: object, data: data)
+    }
+    
+    func delete<ParentObject: Storable, Object: Storable>(parentObject: ParentObject?, object: Object) async throws {
+        try await cloudDatabaseManager.delete(parentObject: parentObject, object: object)
     }
 }
--- Dependencies/User/Sources/UserInterface/UserInterface.swift
+++ Dependencies/User/Sources/UserInterface/UserInterface.swift
@@ -7,6 +7,7 @@
 
 import AuthenticationInterface
 import CloudDatabaseInterface
+import FirebaseAuth
 import Foundation
 
 public struct User: Storable, Hashable {
@@ -14,20 +15,29 @@ public struct User: Storable, Hashable {
     public static var collection: String = "Users"
     
     public let id: String
-    public let providerId: String
+    public let providerId: String?
     public let email: String?
     public let displayName: String?
+    public let photoURL: String?
+    public let topic: String?
+    public let isFirstLogin: Bool
     
     public init(
         id: String = UUID().uuidString,
-        providerId: String,
+        providerId: String?,
         email: String?,
-        displayName: String?
+        displayName: String?,
+        photoURL: String?,
+        topic: String?,
+        isFirstLogin: Bool = true
     ) {
         self.id = id
         self.providerId = providerId
         self.email = email
         self.displayName = displayName
+        self.photoURL = photoURL
+        self.topic = topic
+        self.isFirstLogin = isFirstLogin
     }
     
     public init(from dao: UserDAO) {
@@ -35,6 +45,9 @@ public struct User: Storable, Hashable {
         self.providerId = dao.providerId
         self.email = dao.email
         self.displayName = dao.displayName
+        self.photoURL = dao.photoURL
+        self.topic = dao.topic
+        self.isFirstLogin = dao.isFirstLogin
     }
     
     public init(from authDataResult: AuthenticationDataResult) {
@@ -42,8 +55,20 @@ public struct User: Storable, Hashable {
         self.providerId = authDataResult.providerId
         self.email = authDataResult.email
         self.displayName = authDataResult.displayName
+        self.photoURL = authDataResult.photoURL
+        self.topic = nil
+        self.isFirstLogin = authDataResult.isFirstLogin
     }
     
+    public init(from user: FirebaseAuth.User, isFirstLogin: Bool = true) {
+        self.id = user.uid
+        self.providerId = user.providerID
+        self.email = user.email
+        self.displayName = user.displayName
+        self.photoURL = user.photoURL?.absoluteString
+        self.topic = nil
+        self.isFirstLogin = isFirstLogin
+    }
 }
 
 public struct UserDAO: DAOInterface {
@@ -52,15 +77,21 @@ public struct UserDAO: DAOInterface {
     public var docRef: String?
     
     public let id: String
-    public let providerId: String
+    public let providerId: String?
     public let email: String?
     public let displayName: String?
+    public let photoURL: String?
+    public let topic: String?
+    public let isFirstLogin: Bool
     
     public init(from user: User) {
         self.id = user.id
         self.providerId = user.providerId
         self.email = user.email
         self.displayName = user.displayName
+        self.photoURL = user.photoURL
+        self.topic = user.topic
+        self.isFirstLogin = user.isFirstLogin
     }
 }
 
@@ -68,6 +99,7 @@ public protocol UserManagerInterface {
     var signInResult: AsyncStream<Bool> { get }
     var userUpdates: AsyncThrowingStream<User?, Error> { get }
     
+    func getCurrentUser() throws -> User
     func isUserAuthenticated() throws -> Bool
     func signOut() throws
     func signUp(email: String, password: String) async throws
@@ -77,4 +109,12 @@ public protocol UserManagerInterface {
     func deleteAccount() async throws
     func signInWithGoogle() async throws
     func signInWithFacebook() async throws
+    
+    func create<ParentObject: Storable, Object: Storable>(parentObject: ParentObject?, object: Object) throws
+    func read<ParentObject: Storable, Object: Storable>(parentObject: ParentObject?, object: Object) async throws -> Object
+    func readAll<ParentObject: Storable, Object: Storable>(parentObject: ParentObject?, objectsOfType type: Object.Type) async throws -> [Object]
+    func update<ParentObject: Storable, Object: Storable>(parentObject: ParentObject?, object: Object, data: [String: Any]) throws 
+    func delete<ParentObject: Storable, Object: Storable>(parentObject: ParentObject?, object: Object) async throws
+    func handleFirstLogin() async throws -> Bool 
+    func checkIsFirstLogin() async throws -> Bool
 }
