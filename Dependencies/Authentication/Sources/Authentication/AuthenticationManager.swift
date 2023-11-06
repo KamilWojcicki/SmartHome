dd7d3458b57ca8b0ebe3d0fad6454eac5865c1bd
Kamil Wójcicki <kamilwojcicki@MacBook-Air-Kamil.local>
2023-11-06 11:08:31 +0100

    Modify Authentication Package and delete unusefull functions

--- Dependencies/Authentication/Sources/Authentication/AuthenticationManager.swift
+++ Dependencies/Authentication/Sources/Authentication/AuthenticationManager.swift
@@ -10,80 +10,21 @@ import FirebaseAuth
 
 
 final class AuthenticationManager: AuthenticationManagerInterface {
-    var userID: String = ""
     
     private let auth = Auth.auth()
     private let signInGoogleHelper = SignInGoogleHelper()
     private let signInFacebookHelper  = SignInFacebookHelper()
     
-    init() {
-        userStateListener()
-    }
-    
-    private func userStateListener() {
-        auth.addStateDidChangeListener { [weak self] auth, user in
-            guard let self = self else { return }
-            
-            if let user {
-                self.userID = user.uid
-            }
-        }
-    }
-    
-    func getCurrentUser() throws -> User {
-        try auth.currentUser ?? { throw AuthErrorHandler.getAuthenticatedUserError }()
-    }
-}
-// MARK: Sign In with email
-extension AuthenticationManager {
     func isUserAuthenticated() throws -> Bool {
         guard auth.currentUser != nil else {
             throw AuthErrorHandler.getAuthenticatedUserError
         }
         return true
     }
-    func createUser(email: String, password: String) async throws -> AuthenticationDataResult {
-        do {
-            let user = try await auth.createUser(withEmail: email, password: password).user
-            return AuthenticationDataResult(user: user)
-        } catch {
-            throw AuthErrorHandler.signUpError
-        }
-    }
-    
-    func signInUser(email: String, password: String) async throws -> AuthenticationDataResult {
-        do {
-            let user = try await auth.signIn(withEmail: email, password: password).user
-            return AuthenticationDataResult(user: user)
-        } catch {
-            throw AuthErrorHandler.signInError
-        }
-    }
-    
-    func updatePassword(email: String, password: String, newPassword: String) async throws {
-        guard let user = auth.currentUser else {
-            throw AuthErrorHandler.getAuthenticatedUserError
-        }
-        do {
-            try await user.updatePassword(to: password)
-        } catch {
-            throw AuthErrorHandler.updatePasswordError
-        }
-    }
-    
-    func resetPassword(email: String) async throws {
-        do {
-            try await auth.sendPasswordReset(withEmail: email)
-        } catch {
-            print("Throw an error")
-            throw AuthErrorHandler.resetPasswordError
-        }
-    }
     
     func deleteAccount() async throws {
-        guard let user = auth.currentUser else {
-            throw AuthErrorHandler.getAuthenticatedUserError
-        }
+        let user = try getCurrentUser()
+        
         do {
             try await user.delete()
         } catch {
@@ -99,32 +40,69 @@ extension AuthenticationManager {
         }
     }
 }
+
+// MARK: Sign In with email
+extension AuthenticationManager {
+    func signUp(withEmail email: String, password: String, displayName: String) async throws -> AuthenticationDataResult {
+        do {
+            let user = try await auth.createUser(withEmail: email, password: password).user
+            
+            let changeRequest = auth.currentUser?.createProfileChangeRequest()
+            changeRequest?.displayName = displayName
+            try await changeRequest?.commitChanges()
+            
+            return AuthenticationDataResult(user: user, providerID: user.providerData[0])
+        } catch {
+            throw AuthErrorHandler.signUpError
+        }
+    }
+    
+    func signIn(withEmail email: String, password: String) async throws -> AuthenticationDataResult {
+        do {
+            let user = try await auth.signIn(withEmail: email, password: password).user
+            return AuthenticationDataResult(user: user, userInfo: user.providerData[0])
+        } catch {
+            throw AuthErrorHandler.signInError
+        }
+    }
+    
+    func updatePassword(email: String, password: String, newPassword: String) async throws {
+        let user = try getCurrentUser()
+        do {
+            try await user.updatePassword(to: password)
+        } catch {
+            throw AuthErrorHandler.updatePasswordError
+        }
+    }
+    
+    func resetPassword(withEmail email: String) async throws {
+        do {
+            try await auth.sendPasswordReset(withEmail: email)
+        } catch {
+            print("Throw an error")
+            throw AuthErrorHandler.resetPasswordError
+        }
+    }
+    
+    func getCurrentUser() throws -> User {
+        try auth.currentUser ?? { throw AuthErrorHandler.getAuthenticatedUserError }()
+    }
+}
+
 // MARK: Sign In with SSO
 extension AuthenticationManager {
-    // MARK: PROVIDERS
-    func getProviders() throws -> [AuthenticationInterface.AuthProviderOption] {
-        guard let user = auth.currentUser else {
-            throw AuthErrorHandler.getAuthenticatedUserError
-        }
-        
-        return user
-            .providerData
-            .compactMap { AuthenticationInterface.AuthProviderOption(rawValue: $0.providerID) }
-    }
-    
-    private func signIn(credential: AuthCredential) async throws -> AuthenticationDataResult {
-        do {
-            let user = try await auth.signIn(with: credential).user
-            return AuthenticationDataResult(user: user)
-        } catch {
-            throw AuthErrorHandler.signInWithCredentialError
-        }
-    }
-    
     func signInWithGoogle() async throws -> AuthenticationDataResult {
         do {
-            let credential = try await googleCredential()
-            return try await signIn(credential: credential)
+            let result = try await signInGoogleHelper.signIn()
+            
+            return try await signIn(
+                credential:
+                    GoogleAuthProvider
+                    .credential(
+                        withIDToken: result.idToken,
+                        accessToken: result.accessToken
+                    )
+            )
         } catch {
             throw AuthErrorHandler.signInWithGoogleError
         }
@@ -132,34 +110,26 @@ extension AuthenticationManager {
     
     func signInWithFacebook() async throws -> AuthenticationDataResult {
         do {
-            let credential = try await facebookCredential()
-            return try await signIn(credential: credential)
+            let result = try await signInFacebookHelper.signIn()
+            
+            return try await signIn(
+                credential:
+                    FacebookAuthProvider
+                    .credential(
+                        withAccessToken: result.accessToken
+                    )
+            )
         } catch {
             throw AuthErrorHandler.signInWithFacebookError
         }
     }
-}
-// MARK: Sign in Anonymously
-extension AuthenticationManager {
-    func signInAnonymously() async throws -> AuthenticationDataResult {
-        do {
-            let user = try await auth.signInAnonymously().user
-            return AuthenticationDataResult(user: user)
-        } catch {
-            throw AuthErrorHandler.signInAnonymouslyError
-        }
-        
-    }
-}
-// MARK: CREDENTIALS
-extension AuthenticationManager {
-    private func googleCredential() async throws -> AuthCredential {
-        let result = try await signInGoogleHelper.signIn()
-        return GoogleAuthProvider.credential(withSignInResult: result)
-    }
     
-    private func facebookCredential() async throws -> AuthCredential {
-        let result = try await signInFacebookHelper.signIn()
-        return FacebookAuthProvider.credential(withSignInResult: result)
+    private func signIn(credential: AuthCredential) async throws -> AuthenticationDataResult {
+        do {
+            let user = try await auth.signIn(with: credential).user
+            return AuthenticationDataResult(user: user, userInfo: user.providerData[0])
+        } catch {
+            throw AuthErrorHandler.signInWithCredentialError
+        }
     }
 }
--- Dependencies/Authentication/Sources/AuthenticationInterface/AuthenticationInterface.swift
+++ Dependencies/Authentication/Sources/AuthenticationInterface/AuthenticationInterface.swift
@@ -8,134 +8,112 @@
 import Foundation
 import FirebaseAuth
 
-
-
-//    var initials: String {
-//        let formatter = PersonNameComponentsFormatter()
-//        if let components = formatter.personNameComponents(from: fullname) {
-//            formatter.style = .abbreviated
-//            return formatter.string(from: components)
-//        }
-//        return ""
-//    }
-
-
 public struct AuthenticationDataResult {
     public let uid: String
     public let providerId: String?
     public let email: String?
     public let displayName: String?
     public let photoURL: String?
-    public let isFirstLogin: Bool
     
-    public init(user: User, isFirstLogin: Bool = true) {
+    public init(
+        user: User
+    ) {
         self.uid = user.uid
         self.providerId = user.providerID
         self.email = user.email
         self.displayName = user.displayName
         self.photoURL = user.photoURL?.absoluteString
-        self.isFirstLogin = isFirstLogin
     }
     
     public init(
         user: User,
-        providerId: AuthProviderOption?,
-        isFirstLogin: Bool = true
+        userInfo: UserInfo
     ) {
         self.uid = user.uid
-        self.providerId = providerId?.rawValue
-        self.email = user.email
-        self.displayName = user.displayName
-        self.photoURL = user.photoURL?.absoluteString
-        self.isFirstLogin = isFirstLogin
-    }
-    
-    public init(
-        user: User,
-        userInfo: UserInfo,
-        isFirstLogin: Bool = true
-    ) {
-        self.uid = user.providerData[0].providerID
         self.providerId = userInfo.providerID
         self.email = userInfo.email
         self.displayName = userInfo.displayName
         self.photoURL = userInfo.photoURL?.absoluteString
-        self.isFirstLogin = isFirstLogin
+    }
+    
+    public init(
+        user: User,
+        providerID: UserInfo
+    ) {
+        self.uid = user.uid
+        self.providerId = providerID.providerID
+        self.email = user.email
+        self.displayName = user.displayName
+        self.photoURL = user.photoURL?.absoluteString
     }
 }
 
 public protocol AuthenticationManagerInterface {
-    var userID: String { get }
     
-    func signInAnonymously() async throws -> AuthenticationDataResult
-    
-    func getCurrentUser() throws -> User 
-    func getProviders() throws -> [AuthProviderOption]
+    //Share
     func isUserAuthenticated() throws -> Bool
-    
-    func createUser(email: String, password: String) async throws -> AuthenticationDataResult
-    func signInUser(email: String, password: String) async throws -> AuthenticationDataResult
-    func updatePassword(email: String, password: String, newPassword: String) async throws
-    func resetPassword(email: String) async throws
     func deleteAccount() async throws
+    func signOut() throws
+    
+    //Manage User
+    func signUp(withEmail email: String, password: String, displayName: String) async throws -> AuthenticationDataResult
+    func signIn(withEmail email: String, password: String) async throws -> AuthenticationDataResult
+    func updatePassword(email: String, password: String, newPassword: String) async throws
+    func resetPassword(withEmail email: String) async throws
+    func getCurrentUser() throws -> User
+    
+    //SSO
     func signInWithGoogle() async throws -> AuthenticationDataResult
     func signInWithFacebook() async throws -> AuthenticationDataResult
-    func signOut() throws
-}
-
-public enum AuthProviderOption: String {
-    case email = "password"
-    case google = "google.com"
-    case facebook = "facebook.com"
+    
 }
 
 public enum AuthErrorHandler: LocalizedError {
+    //Share
     case getAuthenticatedUserError
+    case deleteUserError
+    case signOutError
+    
+    //Manage User
     case signUpError
     case signInError
-    case resetPasswordError
     case updatePasswordError
-    case isUserRegisteredError
-    case isEmailInUse
-    case deleteUserError
-    case getProvidersError
-    case signOutError
-    case signInWithCredentialError
+    case resetPasswordError
+    
+    //SSO
     case signInWithGoogleError
     case signInWithFacebookError
-    case signInAnonymouslyError
+    case signInWithCredentialError
+    
     case unknownError(Error)
     
     public var errorDescription: String? {
         switch self {
+        //Shared
         case .getAuthenticatedUserError:
             return "There was a problem with user authenticate"
+        case .deleteUserError:
+            return "There was a problem with deleting user"
+        case .signOutError:
+            return "There was a problem with signing out"
+  
+        // Manage
         case .signUpError:
             return "There was a problem with signing up"
         case .signInError:
             return "There was a problem with signing in"
-        case .resetPasswordError:
-            return "There was a problem with reset user password"
         case .updatePasswordError:
             return "There was a problem with updating user password"
-        case .isUserRegisteredError:
-            return "There was a problem with veryfication is user registered"
-        case .isEmailInUse:
-            return "Email is already in use! Try different email"
-        case .deleteUserError:
-            return "There was a problem with deleting user"
-        case .getProvidersError:
-            return "There was a problem with getting AUTHENTICATION PROVIDERS"
-        case .signOutError:
-            return "There was a problem with signing out"
-        case .signInWithCredentialError:
-            return "There was a problem with credentials"
+        case .resetPasswordError:
+            return "There was a problem with reset user password"
+            
+        // SSO
         case .signInWithGoogleError:
             return "Cannot sign in with google"
         case .signInWithFacebookError:
             return "Cannot sign in with facebook"
-        case .signInAnonymouslyError:
-            return "Cannot sign in anonymously"
+        case .signInWithCredentialError:
+            return "There was a problem with credentials"
         case .unknownError(let error):
             return "Unknow error: \(error.localizedDescription)"
         }
