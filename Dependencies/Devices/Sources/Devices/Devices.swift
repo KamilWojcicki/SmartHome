f8b5c05d3296896810fdaf47168a8b3eb1c170ee
Kamil Wójcicki <kamilwojcicki@MacBook-Air-Kamil.local>
2023-11-15 09:31:51 +0100

    Add Devices Package

--- Dependencies/Devices/Sources/Devices/Devices.swift
+++ Dependencies/Devices/Sources/Devices/Devices.swift
@@ -1,2 +1,27 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import DependencyInjection
+import NavigationInterface
+import SwiftUI
+
+public struct Dependencies {
+    public static func inject() {
+        
+    }
+    
+    public static func injectShared() {
+        Assemblies.resolve(TabCoordinatorInterface.self).register(tab: .devices)
+    }
+}
+
+extension Tab {
+    public static var devices: Tab {
+        Tab(
+            title: "Devices",
+            image: "tv.circle",
+            activeImage: "tv.circle.fill",
+            rootView: AnyView(DeviceView())
+        )
+    }
+}
---
+++ Dependencies/Devices/Sources/DevicesInterface/DevicesInterface.swift
@@ -0,0 +1,8 @@
+//
+//  DevicesInterface.swift
+//  
+//
+//  Created by Kamil Wójcicki on 15/11/2023.
+//
+
+import Foundation
