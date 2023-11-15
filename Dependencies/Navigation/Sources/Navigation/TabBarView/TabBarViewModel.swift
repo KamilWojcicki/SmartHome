1ac5233be5200be71082d97585c589070de24ebb
Kamil Wójcicki <kamilwojcicki@MacBook-Air-Kamil.local>
2023-11-15 09:06:05 +0100

    Add ToolbarItem do Navigation View UserImage

--- Dependencies/Navigation/Sources/Navigation/TabBarView/TabBarViewModel.swift
+++ Dependencies/Navigation/Sources/Navigation/TabBarView/TabBarViewModel.swift
@@ -9,13 +9,15 @@ import DependencyInjection
 import Foundation
 import NavigationInterface
 import SwiftUI
+import UserInterface
 
-
+@MainActor
 final class TabBarViewModel: ObservableObject {
     @Inject private var tabCoordinator: TabCoordinatorInterface
-    
+    @Inject private var userManager: UserManagerInterface
     @Published private(set) var tabs: [Tab] = []
     @Published var selectedTab: String?
+    @Published var userImage: String = ""
     
     init() {
         self.tabs = tabCoordinator.tabs
@@ -25,6 +27,17 @@ final class TabBarViewModel: ObservableObject {
     func tapped(tab: String) {
         withAnimation {
             self.selectedTab = tab
+        }
+    }
+    
+    func getUserImage() {
+        Task {
+            do {
+                let user =  try await userManager.fetchUser()
+                self.userImage = user.photoURL ?? "person.fill"
+            } catch {
+                print(error.localizedDescription)
+            }
         }
     }
 }
