04a6b03dfdfa3876fe2eabae000175be08320263
Kamil Wójcicki <kamilwojcicki@MacBook-Air-Kamil.local>
2023-11-15 09:11:06 +0100

    Change function currentUser in TodoManager

--- Dependencies/ToDo/Sources/ToDo/ToDoManager.swift
+++ Dependencies/ToDo/Sources/ToDo/ToDoManager.swift
@@ -17,58 +17,44 @@ final class ToDoManager: ToDoManagerInterface {
     
     var user: User?
     
-    init() {
-        Task {
-            do {
-                try await getUser()
-            } catch {
-                print(error.localizedDescription)
+    private func currentUser() async throws -> User {
+            if user == nil {
+                self.user = try await userManager.fetchUser()
             }
+            
+            guard let user = user else {
+                throw URLError(.badServerResponse) // custom
+            }
+            return user
         }
-    }
-    
-    private func getUser() async throws {
-        self.user = try await userManager.fetchUser()
-        guard let user = user else {
-            return
-        }
-        print(user)
-    }
-    
-    private func currentUser() throws -> User{
-        guard let user = user else {
-            throw URLError(.badServerResponse)
-        }
-        return user
-    }
     
     //CREATE
     func createToDo(todo: ToDo) async throws {
-        let user = try currentUser()
+        let user = try await currentUser()
         try cloudDatabaseManager.createInSubCollection(parentObject: user, object: todo)
     }
     
     //READ
     func readToDo(todoID: String) async throws -> ToDo {
-        let user = try currentUser()
+        let user = try await currentUser()
         return try await cloudDatabaseManager.readInSubCollection(parentObject: user, documentID: todoID)
     }
     
     //READ ALL
     func readAllToDos() async throws -> [ToDo] {
-        let user = try currentUser()
+        let user = try await currentUser()
         return try await cloudDatabaseManager.readAll(parentObject: user, objectsOfType: ToDo.self)
     }
     
     //UPDATE
     func updateToDo(todo: ToDo, data: [String : Any]) async throws {
-        let user = try currentUser()
+        let user = try await currentUser()
         try await cloudDatabaseManager.updateInSubCollection(parentObject: user, object: todo, data: data)
     }
     
     //DELETE
     func deleteToDo(todo: ToDo) async throws {
-        let user = try currentUser()
+        let user = try await currentUser()
         try await cloudDatabaseManager.delete(parentObject: user, object: todo)
     }
 }
