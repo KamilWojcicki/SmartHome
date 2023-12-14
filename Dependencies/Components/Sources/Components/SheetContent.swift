35dc87fd2d376a55e87d7e993c43e2d1b33e8de7
Kamil Wójcicki <kamilwojcicki@MacBook-Air-Kamil.local>
2023-12-14 20:57:10 +0100

    Add SheetContent Component

--- Dependencies/Components/Sources/Components/SheetContent.swift
+++ Dependencies/Components/Sources/Components/SheetContent.swift
@@ -1,18 +1,80 @@
 //
-//  SwiftUIView.swift
-//  
+//  SheetContent.swift
+//
 //
 //  Created by Kamil Wójcicki on 14/12/2023.
 //
 
 import SwiftUI
+import Design
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct SheetContent: View {
+    public enum Variant {
+        case withField(field: Field, labelButtonText: String, action: () async throws -> Void)
+        case onlyText(text: String)
+    }
+    
+    public enum Field {
+        case text(textFieldText: Binding<String>, placecholder: String)
+        case secure(secureFieldText: Binding<String>, placecholder: String)
+    }
+    
+    private let variant: Variant
+    
+    public init(variant: Variant) {
+        self.variant = variant
+    }
+    
+    public var body: some View {
+        buildContent(for: variant)
+    }
+    
+    @ViewBuilder
+    private func buildContent(for variant: Variant) -> some View {
+        switch variant {
+        case .withField(let field, let labelButtonText, let action):
+            buildViewWithField(with: field, labelButtonText: labelButtonText, action: action)
+        case .onlyText(let text):
+            buildViewWithText(text: text)
+        }
     }
 }
-
-#Preview {
-    SwiftUIView()
+extension SheetContent {
+    @ViewBuilder
+    private func buildViewWithText(text: String) -> some View {
+        ScrollView {
+            VStack {
+                Text(text)
+                    .font(.headline)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func buildViewWithField(with field: Field, labelButtonText: String, action: @escaping () async throws -> Void) -> some View {
+        Text("Change Display Name")
+            .font(.title)
+            .multilineTextAlignment(.center)
+            .bold()
+        
+        switch field {
+        case .text(let textFieldText, let placecholder):
+            TextField(textFieldLogin: textFieldText, placecholder: placecholder)
+        case .secure(let secureFieldText, let placecholder):
+            SecureField(textFieldPassword: secureFieldText, placecholder: placecholder)
+        }
+        
+        Button {
+            Task {
+                do {
+                    try await action()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        } label: {
+            Text(labelButtonText)
+                .withMainButtonViewModifier()
+        }
+    }
 }
